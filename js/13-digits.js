c7fbf53d8635ee626dd9e9e374272b7fec309c4e
Aistė Karaliūnaitė <79445650+AisteKara@users.noreply.github.com>
2021-03-18 21:09:56 +0200

    digits

---
+++ js/13-digits.js
@@ -0,0 +1,32 @@
+function digits(skaicius) {
+    // input validation
+    if (typeof skaicius !== 'number') {
+        return "Pateikta netinkamo tipo reikšmė";
+    }
+    if (isNan(skaicius)) {
+        return 'NaN nera normalus skaicius';
+    } //jei infinity naudojam tai šitą irgi apima
+    if (!isFinite(skaicius)) {
+        return 'Infinity nera normalus skaicius';
+    } // tik čia reikėtų pakeisti į
+    // `${skaicius} nera normalius skaicius`;
+
+
+    // logic
+
+    //return result
+    return 0;
+}
+
+console.log(digits(5), '->', 1);
+console.log(digits(781), '->', 3) );
+console.log(digits(37060123456), '->', 11) );
+console.log(digits(true));
+console.log(digits(Infinity));
+console.log(digits(-Infinity));
+console.log(digits());
+console.log(digits(null));
+console.log(digits(-Infinity));
+
+console.log(digits(“asd”));
+console.log(digits(NaN));
