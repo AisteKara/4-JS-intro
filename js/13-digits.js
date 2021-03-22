c4ca9f4a4ff63a8b0695976f002b5e2dd106f6e1
Aistė Karaliūnaitė <79445650+AisteKara@users.noreply.github.com>
2021-03-22 21:41:36 +0200

    Fixes

--- js/13-digits.js
+++ js/13-digits.js
@@ -8,23 +8,25 @@ function digits(skaicius) {
     } //jei infinity naudojam tai šitą irgi apima
     if (!isFinite(skaicius)) {
         return 'Infinity nera normalus skaicius';
-    } // tik čia reikėtų pakeisti į
-    // `${skaicius} nera normalius skaicius`;
+    } 
+    // tik čia reikėtų pakeisti į
+    // `${skaicius} nera normalius skaicius`
 
 
     // logic
 
     //return result
+
     return 0;
 }
 
 console.log(digits(5), '->', 1);
 console.log(digits(781), '->', 3) );
-console.log(digits(37060123456), '->', 11) );
+console.log(digits(37060123456), '->', 11);
 console.log(digits(true));
 console.log(digits(Infinity));
 console.log(digits(-Infinity));
 console.log(digits());
 console.log(digits(null));
-console.log(digits(“asd”));
+console.log(digits(“text”));
 console.log(digits(NaN));
